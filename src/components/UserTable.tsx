140d954ec32d4620f3b336795bb28dbde5826faf
lns741852 <A0978725050@gmail.com>
2026-06-02 00:46:32 +0800

    filter

--- src/components/UserTable.tsx
+++ src/components/UserTable.tsx
@@ -38,14 +38,59 @@ const UserTable: FC<Props> = ({
             title: 'ID',
             dataIndex: 'id',
             width: 80,
+            sorter: (a, b) => a.age - b.age,
         },
         {
             title: '姓名',
             dataIndex: 'name',
+            filterSearch: true,
+
+            // 過濾器內容
+            filters: list.map(user => ({
+                text: user.name,
+                value: user.name,
+            })),
+
+            //過濾器邏輯
+            onFilter: (value, record) =>
+                record.name
+                    .toLowerCase()
+                    .includes(String(value).toLowerCase()),
+
         },
         {
             title: '年齡',
             dataIndex: 'age',
+            filters: [
+                {
+                    text: '18 以下',
+                    value: 'under18',
+                },
+                {
+                    text: '18~30',
+                    value: '18-30',
+                },
+                {
+                    text: '30 以上',
+                    value: 'over30',
+                },
+            ],
+
+            onFilter: (value, record) => {
+                switch (value) {
+                    case 'under18':
+                        return record.age < 18;
+
+                    case '18-30':
+                        return record.age >= 18 && record.age <= 30;
+
+                    case 'over30':
+                        return record.age > 30;
+
+                    default:
+                        return true;
+                }
+            },
         },
         {
             title: 'Email',
@@ -58,7 +103,20 @@ const UserTable: FC<Props> = ({
         {
             title: '狀態',
             dataIndex: 'status',
+            filters: [
+                {
+                    text: '啟用',
+                    value: 'active',
+                },
+                {
+                    text: '停用',
+                    value: 'inactive',
+                },
+            ],
 
+            onFilter: (value, record) => {
+                return record.status === value;
+            },
             render: (status: UserItem['status']) => {
                 return status === 'active' ? (
                     <Tag color="green">啟用</Tag>
